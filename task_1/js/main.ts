9073aecb0052cc574558de959f1addaf6b5dc593
benjamin-nyankson <117271631+benjamin-nyankson@users.noreply.github.com>
2026-01-29 08:19:01 +0000

    add teacher interface

---
+++ task_1/js/main.ts
@@ -0,0 +1,8 @@
+interface Teacher{
+    readonly firstName:string
+    readonly lastName:string
+    location:string
+    contract:boolean
+    fullTimeEmployee:boolean
+    yearsOfExperience?:number
+}
